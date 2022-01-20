efa4fc04e593e79071086ee801d9c342b4da5da5
Colton <coltonfalkner@gmail.com>
2022-01-20 17:06:03 -0500

    form input created and displayed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
-import ExpenseItem from './components/Expenses/ExpenseItem'
+import React from 'react'
+import NewExpense from './components/NewExpense/NewExpense'
 import Expenses from './components/Expenses/Expenses'
 
 const App = () => {
@@ -26,6 +27,7 @@ const App = () => {
 
   return (
     <div>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   )
